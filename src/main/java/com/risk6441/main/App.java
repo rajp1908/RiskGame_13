77a4816b539bf5330a2b17b19e7f501b14a27e75
Deep Patel <35375843+deep2223@users.noreply.github.com>
2019-02-25 15:53:55 -0500

    Add files via upload
    POM files changed

---
+++ src/main/java/com/risk6441/main/App.java
@@ -0,0 +1,13 @@
+package com.risk6441.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
