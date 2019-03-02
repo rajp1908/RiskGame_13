fd753f7114781e12bed93bb2cbcb989d768d0779
Deep <deeppatel2223@gmail.com>
2019-03-01 19:42:31 -0500

    Player strategy configuration file added

---
+++ src/main/java/com/risk6441/configuration/PlayerStrategy.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.risk6441.config;
+
+/**
+ * The set of named values of for the strategy of the player
+ * @author Deep
+ */
+public enum PlayerStrategy {
+	HUMAN, COMPUTER;
+}
